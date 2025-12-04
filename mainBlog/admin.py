de971ca6d4a8010ef9deab422043ab5981f53439
coderpravin <impravin00@gmail.com>
2025-12-04 17:29:38 +0530

    The Blog and Category Model Create and Connect with Admin Panel

--- mainBlog/admin.py
+++ mainBlog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Category, Blog
 # Register your models here.
+admin.site.register(Category)
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title', )}
+    list_display = ('title','category', 'author','status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'author__username')
+admin.site.register(Blog, BlogAdmin)
