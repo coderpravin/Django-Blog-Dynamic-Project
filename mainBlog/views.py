35a9303d29db1b2c25bf5be1b021f834a6ae54c0
coderpravin <impravin00@gmail.com>
2025-12-11 13:40:11 +0530

    The Log File created and test Success

--- mainBlog/views.py
+++ mainBlog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Blog
+import logging
+
+#create logging here
+logging.basicConfig(filename='sample.log',
+                    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 # Create your views here.
-
 def home(request):
-    return render(request, 'mainBlog/home.html')
+    categories  = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True)
+    logger.debug(f"Home page called")
+    context = {
+         'categories':categories,
+         'featured_post':featured_post
+        }
+    return render(request, 'mainBlog/home.html', context)
+
+
